722eaea888c88aecc4146e17d9af9e215a0a4011
000begana000 <begana.contact@gmail.com>
2025-08-30 13:06:41 +0200

    feat: create reusable Button component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
+import Button from "./UI/Button";
+
 export default function Login() {
   return (
     <div>
       <p>Please enter player name</p>
       <div>
         <input type="text" name="" id="" />
-        <button>save</button>
+        <Button>save</Button>
       </div>
     </div>
   );
