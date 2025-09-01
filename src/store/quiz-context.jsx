16edb46b6eadc39251eaa681f17621b9e421bbc3
000begana000 <begana.contact@gmail.com>
2025-09-01 17:06:24 +0200

    feat: create QuizContext

--- src/store/quiz-context.jsx
+++ src/store/quiz-context.jsx
@@ -1,5 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const QuizContext = createContext({
-  selectedCategoryCode: [],
+  selectedCategoryCode: "",
+  quizItems: [],
+  selectCategoryCode: () => {},
 });
+
+export default function QuizContextProvider({ children }) {
+  const [selectedCategoryCode, setSelectedCategoryCode] = useState("");
+  const [quizItems, setQuizItems] = useState([]);
+
+  useEffect(() => {
+    // prevent too many requests
+    if (!selectedCategoryCode) return;
+
+    fetch(
+      `https://opentdb.com/api.php?amount=10&category=${selectedCategoryCode}&difficulty=easy&type=boolean`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(resData => {
+        setQuizItems(resData.results);
+      });
+  }, [selectedCategoryCode]);
+
+  function handleSelectCategoryCode(codeNum) {
+    setSelectedCategoryCode(codeNum);
+  }
+
+  const ctxValue = {
+    quizItems,
+    selectedCategoryCode,
+    selectCategoryCode: handleSelectCategoryCode,
+  };
+
+  return (
+    <QuizContext.Provider value={ctxValue}>{children}</QuizContext.Provider>
+  );
+}
