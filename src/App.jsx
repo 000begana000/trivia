ecac76a51d8edb35f02f6ca4751259f7ee786607
000begana000 <begana.contact@gmail.com>
2025-09-01 12:47:12 +0200

    feat: create QuizContext and wrap components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
 
+import { QuizContext } from "./store/quiz-context";
+
 function App() {
   return (
-    <>
+    <QuizContext.Provider>
       <Header />
       <Login />
-    </>
+    </QuizContext.Provider>
   );
 }
 
