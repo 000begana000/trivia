2a785167f743a74f683798e7f53415f010235cf5
000begana000 <begana.contact@gmail.com>
2025-09-23 13:12:02 +0200

    feat: start game function

--- src/store/quiz-context.jsx
+++ src/store/quiz-context.jsx
@@ -3,36 +3,53 @@ import { createContext, useState, useEffect } from "react";
 export const QuizContext = createContext({
   selectedCategoryCode: "",
   quizItems: [],
-  selectCategoryCode: () => {},
+  isFetching: "",
+  enteredPlayerName: "",
+  savePlayerName: () => {},
+  startGame: () => {},
 });
 
 export default function QuizContextProvider({ children }) {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("Begana");
   const [selectedCategoryCode, setSelectedCategoryCode] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
   const [quizItems, setQuizItems] = useState([]);
 
   useEffect(() => {
     // prevent too many requests
     if (!selectedCategoryCode) return;
 
+    setIsFetching(true); // loading state
+
     fetch(
       `https://opentdb.com/api.php?amount=10&category=${selectedCategoryCode}&difficulty=easy&type=boolean`
     )
       .then(response => {
+        console.log("fetching");
         return response.json();
       })
       .then(resData => {
         setQuizItems(resData.results);
+        setIsFetching(false); // loading state
       });
   }, [selectedCategoryCode]);
 
-  function handleSelectCategoryCode(codeNum) {
-    setSelectedCategoryCode(codeNum);
+  // save player name
+  function handleSavePlayerName(playerName) {
+    setEnteredPlayerName(playerName);
+  }
+
+  function handleStartGame(categoryId) {
+    setSelectedCategoryCode(categoryId);
   }
 
   const ctxValue = {
     quizItems,
     selectedCategoryCode,
-    selectCategoryCode: handleSelectCategoryCode,
+    isFetching,
+    enteredPlayerName,
+    savePlayerName: handleSavePlayerName,
+    startGame: handleStartGame,
   };
 
   return (
