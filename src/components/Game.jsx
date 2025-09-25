10f5dc7d8d2e9293fa3f6f1d6e86b644da980749
000begana000 <begana.contact@gmail.com>
2025-09-25 11:29:20 +0200

    feat: save user answers and current score according to correct answers

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,24 +5,39 @@ import { decodeHTML } from "../store/htmlDecoder";
 
 import Button from "./UI/Button";
 
+// temporary current score
+let currentScore = 0;
+
 export default function Game() {
-  const { isFetching, quizItems, userAnswers, setUserAnswers } =
-    useContext(QuizContext);
+  const {
+    enteredPlayerName,
+    isFetching,
+    quizItems,
+    userAnswers,
+    setUserAnswers,
+  } = useContext(QuizContext);
+
+  console.log(quizItems);
 
   const activeQuestionIndex = userAnswers.length;
-  console.log(activeQuestionIndex);
 
+  // save user answers & current score
   function handleSetUserAnswers(selectedAsnwer) {
     setUserAnswers(prevAnswers => {
       return [...prevAnswers, selectedAsnwer];
     });
+
+    if (selectedAsnwer === quizItems[activeQuestionIndex].correct_answer) {
+      currentScore += 100;
+      console.log(`current score: ${currentScore}`);
+    }
   }
 
   return (
     <div>
-      <p>username</p>
-      <p>current score</p>
-      <p>high score</p>
+      <p>username: {enteredPlayerName}</p>
+      <p>current score: 0</p>
+      <p>high score: 0</p>
       <ul>
         {isFetching && <p>Loading...</p>}
         <li>
