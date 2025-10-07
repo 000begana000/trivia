26012b76849daee3db898049b1df64fea71da719
000begana000 <begana.contact@gmail.com>
2025-10-07 14:36:45 +0200

    feat: player life system

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,18 @@
-export default function Player({ playerName, currentScore }) {
+import { useState, useEffect } from "react";
+
+export default function Player({ playerName, currentScore, answerState }) {
+  const [playerLife, setPlayerLife] = useState(5);
+
+  useEffect(() => {
+    if (answerState === "wrong") {
+      setPlayerLife(prevLife => prevLife - 1);
+    }
+  }, [answerState]);
+
   return (
     <div>
       <p>username: {playerName}</p>
+      <p>life: {playerLife}</p>
       <p>current score: {currentScore}</p>
       <p>high score: 0</p>
     </div>
