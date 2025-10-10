78211e17e01911450e79900fdc176fb5227ff48c
000begana000 <begana.contact@gmail.com>
2025-10-10 16:06:20 +0200

    feat: consuming quiz context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import QuizContextProvider from "./store/quiz-context";
+
 import Header from "./components/Header";
 import Quiz from "./components/Quiz";
 
 function App() {
   return (
-    <>
+    <QuizContextProvider>
       <Header />
       <Quiz />
-    </>
+    </QuizContextProvider>
   );
 }
 
